62decc8060e870b81c622716d010b1ff2ca5cde2
Osamah Amer <osamah.amer@learnit.fi>
2022-03-24 21:18:15 +0200

    Action creators done

--- src/reducer.js
+++ src/reducer.js
@@ -15,7 +15,10 @@ export default function reducer(state = [], action) {
       ];
     case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
-
+    case actions.BUG_RESOLVED:
+      return state.map((bug) =>
+        bug.id === action.payload.id ? { ...bug, resolved: true } : bug
+      );
     default:
       return state;
   }
