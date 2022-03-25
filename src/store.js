360758f59b0a5be2a4aefa96ee5aa16e4e5429ce
Osamah Amer <osamah.amer@learnit.fi>
2022-03-25 20:24:40 +0200

    CleanUp redux

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import reducer from './reducer'
-import { devToolsEnhancer } from "redux-devtools-extension";
-const store = createStore(reducer, devToolsEnhancer({trace:true})
-);
-
-export default store;
