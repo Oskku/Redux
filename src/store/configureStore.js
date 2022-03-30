8819230697d56f186b11fe8881411e9f3ded85d8
Osamah Amer <osamah.amer@learnit.fi>
2022-03-30 17:29:42 +0300

    Api middleware invoked

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,18 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducers";
 import logger from "./middleware/logger";
-import data from "./middleware/data"
-
+import data from "./middleware/data";
+import api from "./middleware/api";
 /* With toolkit we just import confiureStore and thats it :D */
 export default function configureAppStore() {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger({ destination: "Console" }),data],
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destination: "Console" }),
+      data,
+      api
+      
+    ],
   });
 }
---
+++ src/store/middleware/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+
+
+const api =
+  ({ dispatch }) =>
+  (next) =>
+  async (action) => {
+    if (action.type !== "apiCallBegan") return next(action);
+    next(action);
+    const { url, method, data, onSuccess, onError } = action.payload;
+    try {
+      const response = await axios.request({
+        baseURL: `http://localhost:9001/api/`,
+        url,
+        method,
+        data,
+      });
+      dispatch({type:onSuccess,payload:response.data})
+    } catch (error) {
+      dispatch({
+          type:onError,
+          payload:error
+      })
+    }
+  };
+
+export default api;
